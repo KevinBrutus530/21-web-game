5aa467bfb8a46257f01c01fea6124bf4c0c07d2c
Kevin Brutus <kevinbrutus@pursuit.org>
2019-12-14 11:36:49 -0500

    players score

--- index.js
+++ index.js
@@ -1,23 +1,39 @@
 document.addEventListener("DOMContentLoaded", () => {
     let start = document.querySelector("#start");
     let deck_id;
-    
+    let score = 0;
+    let player = document.querySelector("#player")
+    let dealer = document.querySelector("#dealer")
     const drawCards = async () => {
         try {
             let res = await axios.get("https://deckofcardsapi.com/api/deck/new/");
             deck_id = res.data.deck_id
             let shuffled = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/shuffle/`);
             let draw = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=2`);
+
             for(let i = 0; i < draw.data.cards.length; i++){
-                let img = document.createElement("img");
+                let playerImg = document.createElement("img");
                 let src = draw.data.cards[i].image;
-                img.src = src;
-                document.body.appendChild(img);
+                playerImg.src = src;
+                player.appendChild(playerImg);
             }
+
+            let playerScore = document.createElement("h3")
+            for(let j = 0; j < draw.data.cards.length; j++){
+                if(draw.data.cards[j].value === "KING" || draw.data.cards[j].value === "QUEEN" || draw.data.cards[j].value === "JACK"){
+                    draw.data.cards[j].value = 10 
+                } else if (draw.data.cards[j].value === "ACE"){
+                    draw.data.cards[j].value = 11
+                }
+                score += Number(draw.data.cards[j].value)
+            }
+            playerScore.innerText = `Score: ${score}`
+            player.appendChild(playerScore);  
         }
         catch(err){
             console.log(err)
         }
     }
+
     start.addEventListener("click", drawCards)
 })
